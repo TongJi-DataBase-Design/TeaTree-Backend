eaf3fbe5eca5a2fee3bf21b197e3a2a088e0a57c
汪汪汪 <1209497940@qq.com>
2022-03-29 00:27:09 +0800

    增加第一个API

--- src/main/java/cn/edu/tongji/teatreebackend/controller/ArticleController.java
+++ src/main/java/cn/edu/tongji/teatreebackend/controller/ArticleController.java
@@ -1,10 +1,30 @@
 package cn.edu.tongji.teatreebackend.controller;
 
+import cn.edu.tongji.teatreebackend.entity.TeaDistributionEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
- * TODO:此处写ArticleController类的描述
+ * ArticleController类
+ *
+ * 处理文章所有基础的增删改
  *
  * @author 汪明杰
  * @date 2022/3/29 0:19
  */
+
+@RestController
+@RequestMapping("/api/article")
 public class ArticleController {
+
+    @RequestMapping(value = "/distribution", method = RequestMethod.POST)
+    public ResponseEntity<Boolean> createArticleInTeaDistribution(
+            TeaDistributionEntity teaDistribution
+    ){
+
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
 }
