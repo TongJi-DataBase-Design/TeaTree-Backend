4cb7e8aedb4510017b5014e085de68cfd68ba00e
汪汪汪 <1209497940@qq.com>
2022-03-28 13:45:57 +0800

    初始化项目

---
+++ src/main/java/cn/edu/tongji/teatreebackend/TeaTreeBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.tongji.teatreebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaTreeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaTreeBackendApplication.class, args);
+    }
+
+}
